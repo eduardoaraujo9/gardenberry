22cc12b982276548bb873c7b6bb9ed97492f1977
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-02 20:21:58 -0300

    Coleta tempo atual

---
+++ tempo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import json
+import requests
+import mysql.connector as mysql
+import inspect
+import os
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+configFile = open(path + "/.config.json")
+config = json.loads(configFile.read())
+
+conn = mysql.connect(host=config["mysql"]["host"],user=config["mysql"]["user"],password=config["mysql"]["pass"],database=config["mysql"]["db"])
+sql = conn.cursor()
+
+url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/" + config["api"]["id"] + "/current?token=" + config["api"]["token"]
+
+res = requests.get(url)
+r = json.loads(res.text)
+try:
+	sql.execute("REPLACE INTO gardenberry.tempo (datahora,umidade,temperatura,precipitacao,fonte) VALUES (CONCAT(LEFT(NOW(),14),'00:00'),'" + str(r["data"]["humidity"]) + "','" + str(r["data"]["temperature"]) + "',IF(LEFT('3n',1)>2 AND LEFT('8n',1)<9,(SELECT precipitacao FROM gardenberry.previsao WHERE datahora = CONCAT(LEFT(NOW(),14),'00:00')),0),1);")
+except sql.Error as error:
+	print("Error: {}".format(error))
+
