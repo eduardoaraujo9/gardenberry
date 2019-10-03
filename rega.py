5e2f2e911a117720f8151b973646d0cb6d88a9d4
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-02 21:34:01 -0300

    codigo do POC para debug aprimorado

--- rega.py
+++ rega.py
@@ -25,7 +25,7 @@ try:
 		(SELECT ROUND(AVG(temperatura),1) FROM previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 12 HOUR)) AS fut_temperaturas, \
 		(SELECT SUM(precipitacao) FROM previsao WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR) AND NOW()) AS prev_precipitacoes, \
 		(SELECT ROUND(AVG(temperatura),1) FROM previsao WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR) AND NOW()) AS prev_temperaturas, \
-		ROUND(IFNULL((SELECT MAX(tempo) FROM regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 11 HOUR) AND NOW()),0),0) AS rega, \
+		ROUND(IFNULL((SELECT SUM(tempo) FROM regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 11 HOUR) AND NOW()),0),0) AS rega, \
 		HOUR(NOW()) AS hora \
 		FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR) AND NOW();")
 	r = sql.fetchone()
@@ -39,19 +39,16 @@ try:
 	else:
 		print("evening")
 		if float(r["fut_precipitacoes"]) < 2:
-			t = int(config["rega"]["tempo"])
-			modt1 = float(config["rega"]["start"]) + (float(r["prev_temperaturas"]) *float(config["rega"]["step"]) *2)
-			modu1 = ((100 -float(r["umidades"]))* float(config["rega"]["step"])) + float(config["rega"]["start"])
-			modt2 = float(config["rega"]["start"]) + (float(r["fut_temperaturas"]) *float(config["rega"]["step"]) *2)
-			t = int(float(t) * float(modt1/100) * float(modu1/100) * float(modt2/100))
-			t = t - int(r["rega"])
-			print(str(t) + " " + str(modt1) + " " + str(modu1) + " " + str(modt2))
-			print(str(config["rega"]["tempo"]) + " " + str(r["prev_temperaturas"]) + " " + str(r["umidades"]) + " " + str(r["fut_temperaturas"]))
+			modf = float(2)
 		elif float(r["fut_precipitacoes"]) < 5:
+			modf = float(1)
+		else:
+			modf = 0
+		if modf > 0:
 			t = int(config["rega"]["tempo"])
-			modt1 = float(config["rega"]["start"]) + (float(r["prev_temperaturas"]) *float(config["rega"]["step"]))
-			modu1 = ((100 -float(r["umidades"]))* float(config["rega"]["step"]) /2) + float(config["rega"]["start"])
-			modt2 = float(config["rega"]["start"]) + (float(r["fut_temperaturas"]) *float(config["rega"]["step"]))
+			modt1 = float(config["rega"]["start"]) + (float(r["prev_temperaturas"]) *float(config["rega"]["step"]) *modf)
+			modu1 = ((100 -float(r["umidades"]))* float(config["rega"]["step"]) * float(modf /2)) + float(config["rega"]["start"])
+			modt2 = float(config["rega"]["start"]) + (float(r["fut_temperaturas"]) *float(config["rega"]["step"]) *modf)
 			t = int(float(t) * float(modt1/100) * float(modu1/100) * float(modt2/100))
 			t = t - int(r["rega"])
 			print(str(t) + " " + str(modt1) + " " + str(modu1) + " " + str(modt2))
