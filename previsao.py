d86692d817d16fd8cb0b0872dec31d951a4e9fbc
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-04 19:45:58 -0300

    suporte a mais de uma api key (triangulacao de lugares)

--- previsao.py
+++ previsao.py
@@ -15,14 +15,15 @@ config = json.loads(configFile.read())
 conn = mysql.connect(host=config["mysql"]["host"],user=config["mysql"]["user"],password=config["mysql"]["pass"],database=config["mysql"]["db"])
 sql = conn.cursor()
 
-url = "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/" + config["api"]["id"] + "/hours/72?token=" + config["api"]["token"]
+for api in config["api"]:
+	url = "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/" + str(api["id"]) + "/hours/72?token=" + str(api["token"])
 
-res = requests.get(url)
-r = json.loads(res.text)
-for dados in r["data"]:
-	try:
-		sql.execute("REPLACE INTO gardenberry.previsao (datahora,precipitacao,temperatura) VALUES ('" + str(dados["date"]) + "','" + str(dados["rain"]["precipitation"]) + "','" + str(dados["temperature"]["temperature"]) + "');")
+	res = requests.get(url)
+	r = json.loads(res.text)
+	for dados in r["data"]:
+		try:
+			sql.execute("REPLACE INTO gardenberry.previsao (datahora,precipitacao,temperatura,fonte) VALUES ('" + str(dados["date"]) + "','" + str(dados["rain"]["precipitation"]) + "','" + str(dados["temperature"]["temperature"]) + "','" + str(api["id"]) + "');")
 
-	except sql.Error as error:
-		print("Error: {}".format(error))
+		except sql.Error as error:
+			print("Error: {}".format(error))
 
