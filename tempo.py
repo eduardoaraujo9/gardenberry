39f6f77c31c5dba3f422fcfdb1bc3773c92ce21d
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-04 19:39:36 -0300

    suporte a mais de uma api key (triangulacao de lugares)

--- tempo.py
+++ tempo.py
@@ -15,12 +15,13 @@ config = json.loads(configFile.read())
 conn = mysql.connect(host=config["mysql"]["host"],user=config["mysql"]["user"],password=config["mysql"]["pass"],database=config["mysql"]["db"])
 sql = conn.cursor()
 
-url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/" + config["api"]["id"] + "/current?token=" + config["api"]["token"]
+for api in config["api"]:
+	url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/" + str(api["id"]) + "/current?token=" + str(api["token"])
 
-res = requests.get(url)
-r = json.loads(res.text)
-try:
-	sql.execute("REPLACE INTO gardenberry.tempo (datahora,umidade,temperatura,precipitacao,fonte) VALUES (CONCAT(LEFT(NOW(),14),'00:00'),'" + str(r["data"]["humidity"]) + "','" + str(r["data"]["temperature"]) + "',IF(LEFT('3n',1)>2 AND LEFT('8n',1)<9,(SELECT precipitacao FROM gardenberry.previsao WHERE datahora = CONCAT(LEFT(NOW(),14),'00:00')),0),1);")
-except sql.Error as error:
-	print("Error: {}".format(error))
+	res = requests.get(url)
+	r = json.loads(res.text)
+	try:
+		sql.execute("REPLACE INTO gardenberry.tempo (datahora,umidade,temperatura,precipitacao,fonte) VALUES (CONCAT(LEFT(NOW(),14),'00:00'),'" + str(r["data"]["humidity"]) + "','" + str(r["data"]["temperature"]) + "',IF(LEFT('3n',1)>2 AND LEFT('8n',1)<9,(SELECT precipitacao FROM gardenberry.previsao WHERE datahora = CONCAT(LEFT(NOW(),14),'00:00')),0)," + str(api["id"]) + ");")
+	except sql.Error as error:
+		print("Error: {}".format(error))
 
