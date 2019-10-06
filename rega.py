cea2989da2abd7c698f21fff864873966771c5a5
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-06 19:34:00 -0300

    suporte a multiplos reles

--- rega.py
+++ rega.py
@@ -20,10 +20,12 @@ sql = conn.cursor()
 def rega(tempo):
 	if int(tempo) > 0:
 		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(config["gpio"]["rele"],GPIO.OUT)
-		GPIO.setup(config["gpio"]["rele"],GPIO.LOW)
+		for rele in config["gpio"]["rele"]:
+			GPIO.setup(rele,GPIO.OUT)
+			GPIO.setup(rele,GPIO.LOW)
 		time.sleep(int(tempo))
-		GPIO.setup(config["gpio"]["rele"],GPIO.HIGH)
+		for rele in config["gpio"]["rele"]:
+			GPIO.setup(rele,GPIO.HIGH)
 	sql.execute("INSERT IGNORE INTO gardenberry.regas (datahora, tempo) VALUES (NOW(), " + tempo + ");")
 	print("Regou: " + tempo + " seg")
 
@@ -47,20 +49,20 @@ try:
 			modf = float(1)
 	else:
 		print("evening")
-		if float(r["fut_precipitacoes"]) < 2:
+		if float(r["fut_precipitacoes"]) < (2 *len(config["api"])):
 			modf = float(2)
-		elif float(r["fut_precipitacoes"]) < 5:
+		elif float(r["fut_precipitacoes"]) < (5 *len(config["api"])):
 			modf = float(1)
 	if modf > 0:
 		t = int(config["rega"]["tempo"])
 		modt1 = float(config["rega"]["temperatura"]["start"]) + (float(r["prev_temperaturas"]) *float(config["rega"]["temperatura"]["step"]) *modf)
 		modu1 = ((100 -float(r["umidades"]))* float(config["rega"]["umidade"]["step"]) *modf /2) + float(config["rega"]["umidade"]["start"])
+		modu1 = float(100)
 		modt2 = float(config["rega"]["temperatura"]["start"]) + (float(r["fut_temperaturas"]) *float(config["rega"]["temperatura"]["step"]) *modf)
 		t = int(float(t) * modt1/100 * modu1/100 * modt2/100)
 		t = t - int(r["rega"])
 		print("t\tconf.t\tm_t1\tm_u1\tm_t2\tm_f")
 		print(str(t) + "\t" + str(config["rega"]["tempo"]) + "\t" + str(modt1) + "\t" + str(modu1) + "\t" + str(modt2) + "\t" + str(modf))
-		print(r)
 	if t < 5:
 		t = 0
 # ideia futura, reduzir o tempo da rega matinal (validar acompanhando os logs)
