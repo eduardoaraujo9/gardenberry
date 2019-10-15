069ee7cbc7d0cbce5c7327a23f6d63f97bdb08c4
eduardoaraujo9 <contatoedu@outlook.com>
2019-10-15 17:49:21 -0300

    feito calculo correto para manha

--- rega.py
+++ rega.py
@@ -7,6 +7,7 @@ import inspect
 import os
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 filename = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(filename))
@@ -16,6 +17,8 @@ config = json.loads(configFile.read())
 
 conn = mysql.connect(host=config["mysql"]["host"],user=config["mysql"]["user"],password=config["mysql"]["pass"],database=config["mysql"]["db"])
 sql = conn.cursor(buffered=True)
+
+dt = datetime.datetime.now()
 
 def rega(tempo):
 	if int(tempo) > 0:
@@ -30,38 +33,53 @@ def rega(tempo):
 	print("Regou: " + tempo + " seg")
 
 try:
-	sql.execute("SELECT ROUND(AVG(temperatura),3) AS media FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR);")
+	if dt.hour < 12:
+		sql.execute("SELECT ROUND(AVG(temperatura),3) AS media FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 12 HOUR) AND NOW();")
+		r = sql.fetchone()
+		media = float(r[0])
+		sql.execute("SELECT ROUND(SQRT(SUM(v)/(COUNT(v)-1)),2) AS desvio_padrao FROM ( SELECT temperatura, ROUND((" + str(media) + " -temperatura)*(" + str(media) + " -temperatura),2) AS v FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 12 HOUR) AND NOW() ) AS dp;")
+		r = sql.fetchone()
+		desvio_padrao = float(r[0])
+		sql.execute("SELECT ROUND(AVG(precipitacao),1) AS precipitacoes,ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) AS temperaturas, ROUND(AVG(umidade),0) AS umidades, \
+			(SELECT ROUND(AVG(precipitacao),1) FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 12 HOUR) AND NOW()) AS fut_precipitacoes, \
+			(SELECT ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 12 HOUR) AND NOW()) AS fut_temperaturas, \
+			ROUND(IFNULL((SELECT SUM(tempo) FROM gardenberry.regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 20 HOUR) AND NOW()),0),0) AS rega \
+			FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 12 HOUR) AND NOW();")
+
+	else:
+		sql.execute("SELECT ROUND(AVG(temperatura),3) AS media FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 24 HOUR);")
+		r = sql.fetchone()
+		media = float(r[0])
+		sql.execute("SELECT ROUND(SQRT(SUM(v)/(COUNT(v)-1)),2) AS desvio_padrao FROM ( SELECT temperatura, ROUND((" + str(media) + " -temperatura)*(" + str(media) + " -temperatura),2) AS v FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 24 HOUR) ) AS dp;")
+		r = sql.fetchone()
+		desvio_padrao = float(r[0])
+		sql.execute("SELECT ROUND(AVG(precipitacao),1) AS precipitacoes,ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) AS temperaturas, ROUND(AVG(umidade),0) AS umidades, \
+			(SELECT ROUND(AVG(precipitacao),1) FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 12 HOUR)) AS fut_precipitacoes, \
+			(SELECT ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 12 HOUR)) AS fut_temperaturas, \
+			ROUND(IFNULL((SELECT SUM(tempo) FROM gardenberry.regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 20 HOUR) AND NOW()),0),0) AS rega \
+			FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 24 HOUR) AND NOW();")
+
 	r = sql.fetchone()
-	media = float(r[0])
-	sql.execute("SELECT ROUND(SQRT(SUM(v)/(COUNT(v)-1)),2) AS desvio_padrao FROM ( SELECT temperatura, ROUND((" + str(media) + " -temperatura)*(" + str(media) + " -temperatura),2) AS v FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR) ) AS dp;")
-	r = sql.fetchone()
-	desvio_padrao = float(r[0])
-	sql.execute("SELECT ROUND(AVG(precipitacao),1) AS precipitacoes,ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) AS temperaturas, ROUND(AVG(umidade),0) AS umidades, \
-		(SELECT ROUND(AVG(precipitacao),1) FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 12 HOUR)) AS fut_precipitacoes, \
-		(SELECT ROUND(AVG(temperatura) +" + str(desvio_padrao) + ",1) FROM gardenberry.previsao WHERE datahora BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 12 HOUR)) AS fut_temperaturas, \
-		ROUND(IFNULL((SELECT SUM(tempo) FROM gardenberry.regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 20 HOUR) AND NOW()),0),0) AS rega, \
-		HOUR(NOW()) AS hora \
-		FROM gardenberry.tempo WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL IF(HOUR(NOW())<12,12,24) HOUR) AND NOW();")
-	r = sql.fetchone()
-	column_names = ["precipitacoes","temperaturas","umidades","fut_precipitacoes","fut_temperaturas","rega","hora"]
+	column_names = ["precipitacoes","temperaturas","umidades","fut_precipitacoes","fut_temperaturas","rega"]
 	r = dict(zip(column_names,r))
 	t = 0
 	modf = 0
-	if int(r["hora"]) < 12:
-		print("morning")
-		if int(r["rega"]) == 0 and float(r["precipitacoes"]) < 1.6 and float(r["umidades"]) < 80:
-			modf = float(1)
+
+#	if dt.hour < 12:
+#		print("morning")
+#		if float(r["precipitacoes"]) < 1.6 and float(r["umidades"]) < 80:
+#			modf = float(1)
+#	else:
+#	print("evening")
+	if float(r["fut_precipitacoes"]) < 0.6 and float(r["precipitacoes"]) < 1.6:
+		modf = float(2)
+	elif float(r["fut_precipitacoes"]) < 1.6 and float(r["precipitacoes"]) < 1.6:
+		modf = float(1)
 	else:
-		print("evening")
-		if float(r["fut_precipitacoes"]) < 0.6 and float(r["precipitacoes"]) < 1.6:
-			modf = float(2)
-		elif float(r["fut_precipitacoes"]) < 1.6 and float(r["precipitacoes"]) < 1.6:
-			modf = float(1)
-		else:
-			sql.execute("SELECT ROUND(IFNULL((SELECT SUM(tempo) FROM gardenberry.regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 71 HOUR) AND NOW()),0),0) AS rega;")
-			r = sql.fetchone()
-			if int(r[0]) == 0:
-				modf = 0.7
+		sql.execute("SELECT ROUND(IFNULL((SELECT SUM(tempo) FROM gardenberry.regas WHERE datahora BETWEEN DATE_SUB(NOW(), INTERVAL 71 HOUR) AND NOW()),0),0) AS rega;")
+		r = sql.fetchone()
+		if int(r[0]) == 0:
+			modf = 0.7
 
 	if modf > 0:
 		t = int(config["rega"]["tempo"])
