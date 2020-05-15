5134cd5300e567d31de41517ab1a1be48816da25
eduardoaraujo9 <contatoedu@outlook.com>
2020-05-15 10:58:28 -0300

    minor changes

--- controle.py
+++ controle.py
@@ -2,17 +2,31 @@
 
 import RPi.GPIO as GPIO
 import argparse
+import time
+import random
 
 reles=[23, 24, 25, 27]
 
 parser = argparse.ArgumentParser(description='Liga ou desliga reles')
-parser.add_argument('--on', metavar='N', nargs='+', type=int, help='liga reles N')
-parser.add_argument('--off', metavar='N', nargs='+', type=int, help='desliga reles N')
+parser.add_argument('-on', metavar='N', nargs='+', type=int, help='liga reles N')
+parser.add_argument('-off', metavar='N', nargs='+', type=int, help='desliga reles N')
+parser.add_argument('-sleep', metavar='N', type=int, help='aguarda N segundos')
+parser.add_argument('-rand', metavar='N', type=int, help='aguarda aleatoriamente ate N segundos')
 
 args = parser.parse_args()
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+
+try:
+	time.sleep(args.sleep)
+except:
+	nothing = True
+
+try:
+	time.sleep(random.randint(1,args.rand))
+except:
+	nothing = True
 
 try:
 	for rele in args.on:
